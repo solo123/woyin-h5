c76025d170e001d9943ac2f6b912191fd266e459
gaoqiang19514 <gaoqiang19514@163.com>
2019-03-29 10:46:22 +0800

    祛除非1.0内容

--- src/App.js
+++ src/App.js
@@ -7,22 +7,21 @@ import util from './util'
 import ScrollToTop from './common/ScrollToTop'
 import Auth from './common/Auth'
 
-import Cate from './components/Cate'
-import DoubleColorBall from './components/DoubleColorBall'
-import Product from './components/Product'
-import Buy from './components/Buy'
-
-import RechargeFlow from './components/RechargeFlow'
-import RechargeOil from './components/RechargeOil'
-import RechargeQB from './components/RechargeQB'
-import RechargeVideo from './components/RechargeVideo'
-import RechargeECard from './components/RechargeECard'
-import RechargeECardDetail from './components/RechargeECardDetail'
-import CreditCard from './components/CreditCard'
-import Violation from './components/Violation'
-import ViolationList from './components/ViolationList'
-import ViolationDetail from './components/ViolationDetail'
-import AddVehicle from './components/AddVehicle.js'
+// import Cate from './components/Cate'
+// import DoubleColorBall from './components/DoubleColorBall'
+// import Product from './components/Product'
+// import Buy from './components/Buy'
+// import RechargeFlow from './components/RechargeFlow'
+// import RechargeOil from './components/RechargeOil'
+// import RechargeQB from './components/RechargeQB'
+// import RechargeVideo from './components/RechargeVideo'
+// import RechargeECard from './components/RechargeECard'
+// import RechargeECardDetail from './components/RechargeECardDetail'
+// import CreditCard from './components/CreditCard'
+// import Violation from './components/Violation'
+// import ViolationList from './components/ViolationList'
+// import ViolationDetail from './components/ViolationDetail'
+// import AddVehicle from './components/AddVehicle.js'
 
 import history from './history'
 
@@ -104,24 +103,24 @@ class App extends Component {
             <Auth path="/redeem-record" component={AsyncRedeemRecord} />
             <Auth path="/order" component={AsyncOrder} />
             <Auth path="/recharge-phone" component={AsyncRechargePhone} />
-            
-            <Auth path="/recharge-flow" component={RechargeFlow} />
-            <Auth path="/recharge-oil" component={RechargeOil} />
-            <Auth path="/recharge-qb" component={RechargeQB} />
-            <Auth path="/recharge-video" component={RechargeVideo} />
-            <Auth exact path="/recharge-ecard" component={RechargeECard} />
-            <Auth path="/recharge-ecard/:id" component={RechargeECardDetail} />
-            <Auth path="/credit-card" component={CreditCard} />
-            <Auth exact path="/violation" component={Violation} />
-            <Auth path="/violation/:id" component={ViolationList} />
-            <Auth path="/violation-detail/:id" component={ViolationDetail} />
-            <Auth path="/add-vehicle" component={AddVehicle} />
-            <Auth path="/double-color-ball" component={DoubleColorBall} />
-            
             <Auth path="/transfer" component={AsyncTransfer} />
-            <Route path="/cate" component={Cate} />
-            <Route path="/product" component={Product} />
-            <Route path="/buy/:id" component={Buy} />
+
+            {/* <Auth path="/recharge-flow" component={RechargeFlow} /> */}
+            {/* <Auth path="/recharge-oil" component={RechargeOil} /> */}
+            {/* <Auth path="/recharge-qb" component={RechargeQB} /> */}
+            {/* <Auth path="/recharge-video" component={RechargeVideo} /> */}
+            {/* <Auth exact path="/recharge-ecard" component={RechargeECard} /> */}
+            {/* <Auth path="/recharge-ecard/:id" component={RechargeECardDetail} /> */}
+            {/* <Auth path="/credit-card" component={CreditCard} /> */}
+            {/* <Auth exact path="/violation" component={Violation} /> */}
+            {/* <Auth path="/violation/:id" component={ViolationList} /> */}
+            {/* <Auth path="/violation-detail/:id" component={ViolationDetail} /> */}
+            {/* <Auth path="/add-vehicle" component={AddVehicle} /> */}
+            {/* <Auth path="/double-color-ball" component={DoubleColorBall} /> */}
+            
+            {/* <Route path="/cate" component={Cate} /> */}
+            {/* <Route path="/product" component={Product} /> */}
+            {/* <Route path="/buy/:id" component={Buy} /> */}
             <Route render={ ()=> <AsyncNotFound /> } />
           </Switch>
         </ScrollToTop>
